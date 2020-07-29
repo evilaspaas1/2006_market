3859a261a7dc5edd85414149790d83a665296287
Austin Aspaas <araspaas@live.com>
2020-07-29 10:03:03 -0600

    Add vendors that sell test

--- test/market_test.rb
+++ test/market_test.rb
@@ -51,4 +51,20 @@ class MarketTest < Minitest::Test
     expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
     assert_equal expected, @market.vendor_names
   end
+
+  def test_vendors_that_sell
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    expected = [@vendor1, @vendor3]
+    assert_equal expected, @market.vendors_that_sell(@item1)
+    assert_equal [@vendor2], @market.vendors_that_sell(@item4)
+    assert_equal [@vendor1], @market.vendors_that_sell(@item2)
+    assert_equal [@vendor2], @market.vendors_that_sell(@item3)
+  end
 end
