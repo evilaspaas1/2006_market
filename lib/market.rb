7ccb46010c1baacd7be53dcd7fe80eb009cf8653
Austin Aspaas <araspaas@live.com>
2020-07-29 10:49:36 -0600

    Add total_inventory method and pass test

--- lib/market.rb
+++ lib/market.rb
@@ -26,4 +26,17 @@ class Market
     end
     vendors_that_sell_item
   end
+
+  def total_inventory
+    market_inventory = Hash.new do |market_inventory, item|
+      market_inventory[item] = {quantity: 0, vendors: []}
+    end
+    @vendors.each do |vendor|
+      vendor.inventory.each do |vendor_item, quantity|
+        market_inventory[vendor_item][:quantity] += quantity
+        market_inventory[vendor_item][:vendors] << vendor
+      end
+    end
+    market_inventory
+  end
 end
