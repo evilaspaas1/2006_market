bab9aabc48f3eb05ff9d91e35e3dc066359c7363
Austin Aspaas <araspaas@live.com>
2020-07-29 10:53:21 -0600

    Add overstocked item test

--- test/market_test.rb
+++ test/market_test.rb
@@ -99,4 +99,17 @@ class MarketTest < Minitest::Test
    }
    assert_equal expected_inventory, @market.total_inventory
   end
+
+  def test_it_can_get_overstocked_items
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    assert_equal [@item1], @market.overstocked_items
+  end
 end
